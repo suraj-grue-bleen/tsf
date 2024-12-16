e2fbcc867c669c1b6f1fe732bf68dfcc057e04a0
get-saurya <surajitroy35213@gmail.com>
2024-12-16 22:58:01 +0530

    changes in code

--- backend/src/api/users/users.controller.ts
+++ backend/src/api/users/users.controller.ts
@@ -29,69 +29,16 @@ export class UsersController {
    * @param req
    * @param res
    */
-  // @Get('get-user-list/:id?')
-  // async getUserList(@Req() req: Request, @Res() res: Response) {
-  //   this.usersService.getUsers(req, res);
-  // }
+  
   // @Get('get-user-list/:id?')
   // async findAllUsers(@Req() req: Request, @Res() res: Response): Promise<User[]> {
   //   return this.usersService.getUsers();
   // }
 
+
   
 
-  /**
-   * @param createUserDTO
-   * @param req
-   * @param res
-   * @param transaction
-   *
-   */
-  // @Post('create-user')
-  // async createUser(
-  //   @Req() req: Request,
-  //   @Res() res: Response,
-  //   @Body() createUserDto: CreateUserDto,
-  // ) {
-  //   this.usersService.createUser(req, res);
-  // }
-
-  /**
-   * Update password for logged in user
-   * @param updatePasswordDto
-   * @return response
-   */
-  // @Post('update-password')
-  // async updateAuthenticatedPassword(
-  //   @Body() updatePasswordDTO: UpdatePasswordDto,
-  //   // eslint-disable-next-line prettier/prettier
-  //   @Req() req: Request,
-  //   @Res() res: Response,
-  // ) {
-  //   let authToken = req.user;
-  //   this.usersService.setNewPassword(req, res, authToken);
-  // }
-
-  /**
-   * User Details
-   * @Param req
-   * @Param res
-   */
-  // @Get('get-user-details')
-  // async getUserdetails(@Req() req: Request, @Res() res: Response) {
-  //   let authToken = req.user;
-  //   this.usersService.userDetails(req, res, authToken);
-  // }
-
-  /**
-   * Soft Delete User
-   * @Param req
-   * @return response
-   */
-  // @Post('remove-user')
-  // async removeUser(@Req() req: Request, @Res() res: Response) {
-  //   this.usersService.removeuser(req, res);
-  // }
+ 
 
   /**
    * File/Image upload
